edce87e11cb221ef143cda5aacc20645ea4a8a6e
Sebastien Renault <sre@zenexity.com>
2013-09-05 23:50:17 +0200

    Add forgotten js files

---
+++ Gruntfile.js
@@ -0,0 +1,53 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            options: {
+                forever: true,
+                livereload: true
+            },
+            scripts: {
+                files: ['lib/**/*.ts', 'sample/src/*.ts'],
+                tasks: ['typescript'],
+                options: {
+                }
+            }
+        },
+        typescript: {
+            lib: {
+                src: ['lib/**/*.ts'],
+                dest: 'js/',
+                options: {
+                    module: 'amd',
+                    base_path: 'lib'
+                }
+            },
+            sample: {
+                src: ['sample/src/**/*.ts'],
+                dest: 'sample/javascripts',
+                options: {
+                    module: 'amd',
+                    base_path: 'sample/src'
+                }
+            }
+        },
+        copy: {
+            lib: {
+                cwd: 'lib',
+                src: ['**'],
+                dest: 'sample/src/lib',
+                flatten: true,
+                expand: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Here we  go !
+    grunt.registerTask('default', ['typescript']);
+    grunt.registerTask('lib', ['typescript:lib']);
+    grunt.registerTask('sample', ['copy:lib', 'typescript:sample']);
+};

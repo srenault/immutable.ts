0b85052c967bcae79ec5c378cf383baa4942cf9e
Vincent Munier <vmu@zenexity.com>
2013-09-13 13:06:49 +0200

    All tuples from Tuple2 to Tuple22 are present

--- sample/src/lib/immutable/Tuple.ts
+++ sample/src/lib/immutable/Tuple.ts
@@ -7,3 +7,183 @@ export class Tuple2<T,U> {
         return '(' + this._1 + ',' + this._2 + ')';
     }
 }
+
+export class Tuple3<T,U> {
+    constructor (public _1, public _2, public _3) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ')';
+    }
+}
+
+export class Tuple4<T,U> {
+    constructor (public _1, public _2, public _3, public _4) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ')';
+    }
+}
+
+export class Tuple5<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ')';
+    }
+}
+
+export class Tuple6<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ')';
+    }
+}
+
+export class Tuple7<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ')';
+    }
+}
+
+export class Tuple8<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ')';
+    }
+}
+
+export class Tuple9<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ')';
+    }
+}
+
+export class Tuple10<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ')';
+    }
+}
+
+export class Tuple11<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ')';
+    }
+}
+
+export class Tuple12<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ')';
+    }
+}
+
+export class Tuple13<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ')';
+    }
+}
+
+export class Tuple14<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ')';
+    }
+}
+
+export class Tuple15<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ')';
+    }
+}
+
+export class Tuple16<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15, public _16) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ',' + this._16 + ')';
+    }
+}
+
+export class Tuple17<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15, public _16, public _17) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ',' + this._16 + ',' + this._17 + ')';
+    }
+}
+
+export class Tuple18<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15, public _16, public _17, public _18) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ',' + this._16 + ',' + this._17 + ',' + this._18 + ')';
+    }
+}
+
+export class Tuple19<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15, public _16, public _17, public _18, public _19) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ',' + this._16 + ',' + this._17 + ',' + this._18 + ',' + this._19 + ')';
+    }
+}
+
+export class Tuple20<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15, public _16, public _17, public _18, public _19, public _20) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ',' + this._16 + ',' + this._17 + ',' + this._18 + ',' + this._19 + ',' + this._20 + ')';
+    }
+}
+
+export class Tuple21<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15, public _16, public _17, public _18, public _19, public _20, public _21) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ',' + this._16 + ',' + this._17 + ',' + this._18 + ',' + this._19 + ',' + this._20 + ',' + this._21 + ')';
+    }
+}
+
+export class Tuple22<T,U> {
+    constructor (public _1, public _2, public _3, public _4, public _5, public _6, public _7, public _8, public _9, public _10, public _11, public _12, public _13, public _14, public _15, public _16, public _17, public _18, public _19, public _20, public _21, public _22) {
+    }
+
+    public toString() {
+        return '(' + this._1 + ',' + this._2 + ',' + this._3 + ',' + this._4 + ',' + this._5 + ',' + this._6 + ',' + this._7 + ',' + this._8 + ',' + this._9 + ',' + this._10 + ',' + this._11 + ',' + this._12 + ',' + this._13 + ',' + this._14 + ',' + this._15 + ',' + this._16 + ',' + this._17 + ',' + this._18 + ',' + this._19 + ',' + this._20 + ',' + this._21 + ',' + this._22 + ')';
+    }
+}
